3fcc39926632c6217bb40ca22201ff440d2c6de9
hehh <lingtinzhe@icloud.com>
2020-03-14 01:49:57 +0800

    初始化项目

---
+++ common-bean/src/main/java/com/hehh/cloud/common/bean/login/Auth.java
@@ -0,0 +1,21 @@
+package com.hehh.cloud.common.bean.login;
+
+import lombok.Data;
+
+/**
+ * @author : HeHui
+ * @date : 2019-03-07 12:18
+ * @describe : 用户授权（权限列表）
+ */
+@Data
+public class Auth implements java.io.Serializable {
+
+    /**权限url*/
+    private String url;
+
+    /**请求方法*/
+    private String requestMethod;
+
+    /**权限名称*/
+    private String authName;
+}
