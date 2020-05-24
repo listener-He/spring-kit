9ef2d982ec7df5ecd1327404b0a914b9191ce430
hehh <lingtinzhe@icloud.com>
2020-05-25 00:45:58 +0800

    添加mybatis-plus Repository

--- db-manager/orm-repository-jpa/src/main/java/org/hehh/repository/DbJpaRepository.java
+++ db-manager/orm-repository-jpa/src/main/java/org/hehh/repository/DbJpaRepository.java
@@ -1,11 +1,25 @@
 package org.hehh.repository;
 
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author: HeHui
  * @date: 2020-05-04 21:02
  * @description: spring-data-jpa 资源库
  */
-public interface DbJpaRepository<T,ID>  extends JpaRepository<T,ID>,Repository<T> {
+public interface DbJpaRepository<T,ID,S extends T>  extends JpaRepository<T,ID>, Repository<T,ID,Example<S>> {
+
+
+
 }
--- db-manager/orm-repository-mybatis/src/main/java/org/hehh/repository/MybatisRepository.java
+++ db-manager/orm-repository-mybatis/src/main/java/org/hehh/repository/MybatisRepository.java
@@ -1,11 +1,12 @@
 package org.hehh.repository;
 
 import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.entity.Example;
 
 /**
  * @author: HeHui
  * @date: 2020-05-05 21:52
  * @description: mybatis 资源库
  */
-public interface MybatisRepository<T> extends Mapper<T>,Repository<T> {
+public interface MybatisRepository<T,I> extends Mapper<T>,Repository<T,I, Example> {
 }
