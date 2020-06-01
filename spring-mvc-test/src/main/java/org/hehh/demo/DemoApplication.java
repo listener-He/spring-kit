0856680dc2809c9b57fd18b042dd18ae8463e5b7
hehh <lingtinzhe@icloud.com>
2020-06-01 14:31:29 +0800

    找回文件

---
+++ spring-mvc-test/src/main/java/org/hehh/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package org.hehh.demo;
+
+import org.hehh.cloud.spring.mvc.annotation.EnableEnhanceRequest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: HeHui
+ * @create: 2020-03-22 00:56
+ * @description: demo程序启动
+ **/
+@SpringBootApplication
+@EnableEnhanceRequest
+public class DemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
