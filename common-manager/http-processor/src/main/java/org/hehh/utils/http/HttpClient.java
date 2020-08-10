e3dd86c21712e5e21a9f6073ba58edb39b04745b
hehh <lingtinzhe@icloud.com>
2020-08-11 00:19:46 +0800

    添加原生请求工具

---
+++ common-manager/http-processor/src/main/java/org/hehh/utils/http/HttpClient.java
@@ -0,0 +1,13 @@
+package org.hehh.utils.http;
+
+/**
+ * @author: HeHui
+ * @date: 2020-08-11 00:18
+ * @description: http 请求工具
+ *  参考
+ *   1. https://blog.csdn.net/u012836851/article/details/106373862
+ *   2. https://blog.csdn.net/qq_33212500/article/details/79229822
+ *   3. https://www.cnblogs.com/hhhshct/p/8523697.html
+ */
+public class HttpClient {
+}
