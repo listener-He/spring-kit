50e9c2ba3f333a12b0624ceba0033a317895b774
hehh <lingtinzhe@icloud.com>
2020-09-15 01:24:00 +0800

    补充元注解

--- spring-boot-extend-manager/spring-boot-extend-mvc/spring-boot-extend-mvc-core/src/main/java/org/hehh/cloud/spring/mvc/sign/Signature.java
+++ spring-boot-extend-manager/spring-boot-extend-mvc/spring-boot-extend-mvc-core/src/main/java/org/hehh/cloud/spring/mvc/sign/Signature.java
@@ -1,10 +1,15 @@
 package org.hehh.cloud.spring.mvc.sign;
 
+import java.lang.annotation.*;
+
 /**
  * @author: HeHui
  * @date: 2020-09-14 22:34
  * @description: API签名注解标示
  */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface Signature {
 
 
