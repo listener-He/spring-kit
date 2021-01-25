7348651a9e0141d4d657f01508466bfcd174bcd3
hehh <lingtinzhe@icloud.com>
2021-01-25 10:43:55 +0800

    完成ftl to html

--- common-manager/spring-boot-file-tool/src/main/java/org/hehh/utils/file/ftl/DefaultGeneratorHtmlHandler.java
+++ common-manager/spring-boot-file-tool/src/main/java/org/hehh/utils/file/ftl/DefaultGeneratorHtmlHandler.java
@@ -4,10 +4,8 @@ import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import freemarker.template.Version;
-import org.hehh.cloud.common.bean.result.Result;
 
-import java.io.IOException;
-import java.io.Writer;
+import java.io.*;
 import java.util.Map;
 
 /**
@@ -18,26 +16,56 @@ import java.util.Map;
 public class DefaultGeneratorHtmlHandler implements GeneratorHtmlHandler {
 
     private final Configuration defaultConfiguration;
-    private final ConfigurationBuilderFactory configurationBuilderFactory;
-    private final Version defaultVersion = Configuration.VERSION_2_3_0;
 
+    private final ConfigurationBuilderFactory configurationBuilderFactory;
+
+
+    /**
+     * ftl 默认的版本
+     */
+    private final Version defaultVersion;
+
+    /**
+     * 默认生成器html处理程序
+     *
+     * @param configurationBuilderFactory 配置构建器工厂
+     */
     public DefaultGeneratorHtmlHandler(ConfigurationBuilderFactory configurationBuilderFactory) {
-        this.configurationBuilderFactory = configurationBuilderFactory;
-        this.defaultConfiguration = configurationBuilderFactory.builder(this.getClass(), "template", defaultVersion);
+        this(configurationBuilderFactory,"template");
     }
 
     /**
      * 默认生成器html处理程序
      *
-     * @param builder 构建器
-     *
-     * @throws IOException ioexception
+     * @param configurationBuilderFactory 配置构建器工厂
+     * @param classLoadTemp               类加载模板目录
      */
-    public DefaultGeneratorHtmlHandler(ConfigurationBuilder builder, ConfigurationBuilderFactory configurationBuilderFactory) throws IOException {
-        defaultConfiguration = builder;
-        this.configurationBuilderFactory = configurationBuilderFactory;
+    public DefaultGeneratorHtmlHandler(ConfigurationBuilderFactory configurationBuilderFactory,String classLoadTemp) {
+        this(configurationBuilderFactory,classLoadTemp,Configuration.VERSION_2_3_0);
     }
 
+    /**
+     * 默认生成器html处理程序
+     *
+     * @param configurationBuilderFactory 配置构建器工厂
+     * @param defaultVersion              默认的版本
+     */
+    public DefaultGeneratorHtmlHandler(ConfigurationBuilderFactory configurationBuilderFactory,Version defaultVersion) {
+        this(configurationBuilderFactory,"template",defaultVersion);
+    }
+
+    /**
+     * 默认生成器html处理程序
+     *
+     * @param configurationBuilderFactory 配置构建器工厂
+     * @param classLoadTemp               类加载模板目录
+     * @param defaultVersion              默认的版本
+     */
+    public DefaultGeneratorHtmlHandler(ConfigurationBuilderFactory configurationBuilderFactory,String classLoadTemp,Version defaultVersion) {
+        this.configurationBuilderFactory = configurationBuilderFactory;
+        this.defaultVersion = defaultVersion;
+        this.defaultConfiguration = configurationBuilderFactory.builder(this.getClass(), classLoadTemp, defaultVersion);
+    }
 
     /**
      * 默认生成器html处理程序
@@ -47,18 +75,158 @@ public class DefaultGeneratorHtmlHandler implements GeneratorHtmlHandler {
      * @throws IOException ioexception
      */
     public DefaultGeneratorHtmlHandler(Configuration configuration, ConfigurationBuilderFactory configurationBuilderFactory) throws IOException {
+        this(configuration,configurationBuilderFactory,Configuration.VERSION_2_3_0);
+    }
+
+
+    /**
+     * 默认生成器html处理程序
+     *
+     * @param configuration               配置
+     * @param configurationBuilderFactory 配置构建器工厂
+     * @param defaultVersion              默认的版本
+     *
+     * @throws IOException ioexception
+     */
+    public DefaultGeneratorHtmlHandler(Configuration configuration, ConfigurationBuilderFactory configurationBuilderFactory,Version defaultVersion) throws IOException {
         defaultConfiguration = configuration;
+        this.defaultVersion = defaultVersion;
         this.configurationBuilderFactory = configurationBuilderFactory;
     }
 
 
-
-
-
-
+    /**
+     * 生成
+     *
+     * @param source       源目录
+     * @param params       参数
+     * @param filename     模版名
+     * @param outputStream 输出到流
+     *
+     * @throws IOException       输入输出文件IO异常
+     * @throws TemplateException 生成模板异常
+     */
+    @Override
+    public void generator(String source, Map<String, Object> params, String filename, OutputStream outputStream) throws IOException, TemplateException {
+        generator(configurationBuilderFactory.builder(source, defaultVersion), filename, params, new OutputStreamWriter(outputStream));
+    }
 
     /**
-     *  生成
+     * 生成
+     *
+     * @param filename     模版名
+     * @param params       参数
+     * @param outputStream 输出到流
+     *
+     * @throws IOException       输入输出文件IO异常
+     * @throws TemplateException 生成模板异常
+     */
+    @Override
+    public void generator(String filename, Map<String, Object> params, OutputStream outputStream) throws IOException, TemplateException {
+        generator(defaultConfiguration, filename, params, new OutputStreamWriter(outputStream));
+    }
+
+    /**
+     * 生成
+     *
+     * @param source   源
+     * @param params   参数
+     * @param filename 模板名
+     * @param outFile  to文件
+     *
+     * @throws IOException       输入输出文件IO异常
+     * @throws TemplateException 生成模板异常
+     */
+    @Override
+    public void generator(String source, Map<String, Object> params, String filename, File outFile) throws IOException, TemplateException {
+        generator(configurationBuilderFactory.builder(source, defaultVersion), filename, params, new FileWriter(outFile));
+    }
+
+    /**
+     * 生成
+     *
+     * @param params   参数
+     * @param filename 模版名
+     * @param outFile  to文件
+     *
+     * @throws IOException       输入输出文件IO异常
+     * @throws TemplateException 生成模板异常
+     */
+    @Override
+    public void generator(Map<String, Object> params, String filename, File outFile) throws IOException, TemplateException {
+        generator(defaultConfiguration, filename, params, new FileWriter(outFile));
+    }
+
+    /**
+     * 生成
+     *
+     * @param source   源
+     * @param params   参数
+     * @param filename 模板名
+     *
+     * @return {@link String} 输出到字符串
+     *
+     * @throws IOException       输入输出文件IO异常
+     * @throws TemplateException 生成模板异常
+     */
+    @Override
+    public String generator(String source, Map<String, Object> params, String filename) throws IOException, TemplateException {
+        StringWriter writer = new StringWriter();
+        generator(configurationBuilderFactory.builder(source, defaultVersion), filename, params, writer);
+        return writer.toString();
+    }
+
+    /**
+     * 生成
+     *
+     * @param params   参数
+     * @param filename 模版名
+     *
+     * @return {@link String} 输出到字符串
+     *
+     * @throws IOException       输入输出文件IO异常
+     * @throws TemplateException 生成模板异常
+     */
+    @Override
+    public String generator(Map<String, Object> params, String filename) throws IOException, TemplateException {
+        StringWriter writer = new StringWriter();
+        generator(defaultConfiguration, filename, params, writer);
+        return writer.toString();
+    }
+
+    /**
+     * 生成
+     *
+     * @param params   参数
+     * @param filename 模板名
+     * @param out      输出
+     *
+     * @throws IOException       输入输出文件IO异常
+     * @throws TemplateException 生成模板异常
+     */
+    @Override
+    public void generator(Map<String, Object> params, String filename, Writer out) throws IOException, TemplateException {
+        generator(defaultConfiguration, filename, params, out);
+    }
+
+    /**
+     * 生成
+     *
+     * @param source   源
+     * @param params   参数
+     * @param filename 模板名
+     * @param out      输出
+     *
+     * @throws IOException       输入输出文件IO异常
+     * @throws TemplateException 生成模板异常
+     */
+    @Override
+    public void generator(String source, Map<String, Object> params, String filename, Writer out) throws IOException, TemplateException {
+        generator(configurationBuilderFactory.builder(source, defaultVersion), filename, params, out);
+    }
+
+    /**
+     * 生成
      *
      * @param configuration 配置
      * @param filename      文件名
--- common-manager/spring-boot-file-tool/src/main/java/org/hehh/utils/file/ftl/GeneratorHtmlHandler.java
+++ common-manager/spring-boot-file-tool/src/main/java/org/hehh/utils/file/ftl/GeneratorHtmlHandler.java
@@ -1,8 +1,10 @@
 package org.hehh.utils.file.ftl;
 
 
-import java.io.File;
-import java.io.OutputStream;
+import freemarker.template.TemplateException;
+import org.springframework.util.ResourceUtils;
+
+import java.io.*;
 import java.util.Map;
 
 /**
@@ -16,48 +18,122 @@ public interface GeneratorHtmlHandler {
     /**
      * 生成
      *
-     * @param source   源目录
-     * @param params   参数
-     * @param filename 模版名
-     *
-     * @return {@link OutputStream}
+     * @param source       源目录
+     * @param params       参数
+     * @param filename     模版名
+     * @param outputStream 输出到流
+     * @throws IOException 输入输出文件IO异常
+     * @throws TemplateException 生成模板异常
      */
-    OutputStream generator(String source, Map<String, Object> params, String filename);
+    void generator(String source, Map<String, Object> params, String filename, OutputStream outputStream) throws IOException, TemplateException;
 
     /**
      * 生成
      *
-     * @param filename   模版名
-     * @param params 参数
-     *
-     * @return {@link OutputStream }
-     */
-    OutputStream generator(String filename, Map<String, Object> params);
-
-
-    /**
-     *  生成
-     *
-     * @param source       源
+     * @param filename     模版名
      * @param params       参数
-     * @param filename     模板名
-     * @param outFile to文件
-     *
-     * @return {@link File}
+     * @param outputStream 输出到流
+     * @throws IOException 输入输出文件IO异常
+     * @throws TemplateException 生成模板异常
      */
-    File generator(String source, Map<String, Object> params, String filename, String outFile);
+    void generator(String filename, Map<String, Object> params, OutputStream outputStream) throws IOException, TemplateException;
 
     /**
-     *  生成
+     * 生成
      *
-     * @param params       参数个数
-     * @param filename     文件名
-     * @param outFile to文件
-     *
-     * @return {@link File}
+     * @param source   源
+     * @param params   参数
+     * @param filename 模板名
+     * @param outFile  to文件
+     * @throws IOException 输入输出文件IO异常
+     * @throws TemplateException 生成模板异常
      */
-    File generator(Map<String, Object> params, String filename, String outFile);
+    default void generator(String source, Map<String, Object> params, String filename, String outFile) throws IOException, TemplateException {
+        generator(source, params, filename, ResourceUtils.getFile(outFile));
+    }
 
+    /**
+     * 生成
+     *
+     * @param source   源
+     * @param params   参数
+     * @param filename 模板名
+     * @param outFile  to文件
+     * @throws IOException 输入输出文件IO异常
+     * @throws TemplateException 生成模板异常
+     */
+    void generator(String source, Map<String, Object> params, String filename, File outFile) throws IOException, TemplateException;
+
+    /**
+     * 生成
+     *
+     * @param params   参数
+     * @param filename 模版名
+     * @param outFile  to文件
+     * @throws IOException 输入输出文件IO异常
+     * @throws TemplateException 生成模板异常
+     */
+    default void generator(Map<String, Object> params, String filename, String outFile) throws IOException, TemplateException {
+        generator(params, filename, ResourceUtils.getFile(outFile));
+    }
+
+    /**
+     * 生成
+     *
+     * @param params   参数
+     * @param filename 模版名
+     * @param outFile  to文件
+     * @throws IOException 输入输出文件IO异常
+     * @throws TemplateException 生成模板异常
+     */
+    void generator(Map<String, Object> params, String filename, File outFile) throws IOException, TemplateException;
+
+
+    /**
+     * 生成
+     *
+     * @param source   源
+     * @param params   参数
+     * @param filename 模板名
+     * @throws IOException 输入输出文件IO异常
+     * @throws TemplateException 生成模板异常
+     * @return {@link String} 输出到字符串
+     */
+    String generator(String source, Map<String, Object> params, String filename) throws IOException, TemplateException;
+
+    /**
+     * 生成
+     *
+     * @param params   参数
+     * @param filename 模版名
+     * @throws IOException 输入输出文件IO异常
+     * @throws TemplateException 生成模板异常
+     * @return {@link String} 输出到字符串
+     */
+    String generator(Map<String, Object> params, String filename) throws IOException, TemplateException;
+
+    /**
+     * 生成
+     *
+     * @param params   参数
+     * @param filename 模板名
+     * @param out      输出
+     * @throws IOException 输入输出文件IO异常
+     * @throws TemplateException 生成模板异常
+     */
+    void generator(Map<String, Object> params, String filename, Writer out) throws IOException, TemplateException;
+
+    /**
+     * 生成
+     *
+     * @param source   源
+     * @param params   参数
+     * @param filename 模板名
+     * @param out      输出
+     * @throws IOException 输入输出文件IO异常
+     * @throws TemplateException 生成模板异常
+     */
+    void generator(String source, Map<String, Object> params, String filename, Writer out) throws IOException, TemplateException;
 
 
 }
